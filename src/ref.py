216a4c00036248e3d8c0dbcd7c1d0c8261b2a916
Martin <martinbog19@gmail.com>
2025-10-18 17:38:48 -0400

    temp work

---
+++ src/ref.py
@@ -0,0 +1,21 @@
+EMAILS = {
+    "Alexis": "alexis.bogaert2309@gmail.com",
+    "Martin": "martinbog19@gmail.com",
+    "Thomas": "tbogaert19@gmail.com",
+    "Lucas": "",
+    "Krish": "",
+}
+
+PLAYERS = {
+    "nba": [
+        "Krish",
+        "Lucas",
+        "Martin",
+        "Thomas",
+    ],
+    "prem": [
+        "Alexis",
+        "Martin",
+        "Thomas",
+    ]
+}
