1a070ac5a95bece602bfefa84c0204164b382779
Martin <martinbog19@gmail.com>
2025-10-23 10:32:04 -0400

    test

--- test.py
+++ test.py
@@ -1,19 +1,18 @@
 import time
 from nba_api.stats.endpoints import leaguestandings
-from nba_api.library.http import NBAStatsHTTP
+# from nba_api.library.http import NBAStatsHTTP
 from requests.exceptions import ReadTimeout
 
-# Monkey-patch NBAStatsHTTP to use friendlier headers
-NBAStatsHTTP().session.headers.update({
+custom_headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
     "Accept": "application/json, text/plain, */*",
     "Origin": "https://www.nba.com",
     "Referer": "https://www.nba.com/",
-})
+}
 
 for attempt in range(5):
     try:
-        ls_api = leaguestandings.LeagueStandings(season="2025-26", timeout=60)
+        ls_api = leaguestandings.LeagueStandings(season="2025-26", timeout=60, headers=custom_headers)
         data = ls_api.get_dict()
         print(data)
         break
