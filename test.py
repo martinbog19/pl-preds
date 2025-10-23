f8b9fbe8e7b77437ce6b400ff6b59882ba33d1ec
Martin <martinbog19@gmail.com>
2025-10-23 10:54:26 -0400

    try yahoo

--- test.py
+++ test.py
@@ -1,23 +1,8 @@
-import time
-from nba_api.stats.endpoints import leaguestandings
-# from nba_api.library.http import NBAStatsHTTP
-from requests.exceptions import ReadTimeout
+from bs4 import BeautifulSoup
+import requests
 
-custom_headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-    "Accept": "application/json, text/plain, */*",
-    "Origin": "https://www.nba.com",
-    "Referer": "https://www.nba.com/",
-}
-
-for attempt in range(5):
-    try:
-        ls_api = leaguestandings.LeagueStandings(season="2025-26", timeout=60, headers=custom_headers)
-        data = ls_api.get_dict()
-        print(data)
-        break
-    except ReadTimeout:
-        print(f"Timeout on attempt {attempt + 1}. Retrying in 10 seconds...")
-        time.sleep(10)
-else:
-    raise Exception("Failed to fetch data after 5 attempts.")
+url = "https://sports.yahoo.com/nba/standings/?selectedTab=CONFERENCE"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
+tables = soup.find_all("table")
+print(tables[-1])
