11894637943551a792ab06bc1c2403b176414cae
Martin <martinbog19@gmail.com>
2025-11-06 23:02:47 -0500

    save artifacts, delete old stuff

--- test.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = "https://sports.yahoo.com/nba/standings/?selectedTab=CONFERENCE"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
-tables = soup.find_all("table")
-print(tables[-1])
